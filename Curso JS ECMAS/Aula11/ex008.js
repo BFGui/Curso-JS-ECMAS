a81766c4dd2f0df8e77e7d35c14186e595e4e76b
BFGui <84551468+BFGui@users.noreply.github.com>
2025-04-03 21:31:33 +0000

    Primeiro Backup

---
+++ Curso JS ECMAS/Aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 45
+console.log(`A velocidade do seu carro e ${vel}Km/h`)
+
+
+
+if (vel > 60) {
+    console.log(`Voce ultrapassou a velocidade permitida. Multado!`)
+}
+console.log(`Dirija sempre usando o cinto de seguranca.`)
